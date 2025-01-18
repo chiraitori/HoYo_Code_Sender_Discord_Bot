8424d862a75d1e2a84aede3f206de5d5c47cb606
Chiraitori <ggaff88@gmail.com>
2025-01-18 13:57:39 +0700

    intent not using for the app need to to vertify the app

--- index.js
+++ index.js
@@ -70,9 +70,8 @@ const client = new Client({
     intents: [
         GatewayIntentBits.Guilds,
         GatewayIntentBits.GuildMessages,
-        GatewayIntentBits.MessageContent,
+        //GatewayIntentBits.MessageContent,
         GatewayIntentBits.GuildMembers,
-        GatewayIntentBits.GuildPresences
     ]
 });
 
